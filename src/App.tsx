515acdced37c826de229c143d30582e97835ec6d
Omaribz <cynthiamyud@gmail.com>
2024-06-21 04:55:50 -0700

    Setup the folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline mb-5">Hello world!</h1>
-      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-secondary">Secondary</button>
     </>
   );
 }
