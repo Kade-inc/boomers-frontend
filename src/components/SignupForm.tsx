c1815008f6c0963b2a1cd163dfa29ae85b1557bf
MatoG50 <matog50@gmail.com>
2024-07-02 21:20:29 +0300

    sign up form edits

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,59 +1,82 @@
+import { FcGoogle } from "react-icons/fc";
+
 const SignupForm = () => {
   return (
     <div className="text-[#393E46]">
-      <form className="p-8 w-full">
-        <h1>SIGN UP</h1>
-        <p>Create an account to begin your journey</p>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="name">
+      <form className="w-full px-24 py-[2%]">
+        <p className="font-extrabold text-[50px] leading-[53px]">SIGN UP</p>
+        <p className="py-[2.6%] text-[25px] font-semibold">
+          Create an account to begin your journey
+        </p>
+        <div className="mb-[3%]">
+          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
             Email
           </label>
           <input
             type="text"
             placeholder="Enter your email"
-            className="input input-bordered w-full"
+            className="input w-full border border-gray-700 bg-transparent rounded-md"
+            style={{ backgroundColor: "transparent" }}
           />
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="name">
+        <div className="mb-[3%]">
+          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
             Username
           </label>
           <input
             type="text"
             placeholder="Enter your username"
-            className="input input-bordered w-full"
+            className="input w-full border border-gray-700 bg-transparent rounded-md"
+            style={{ backgroundColor: "transparent" }}
           />
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="name">
+        <div className="mb-[3%]">
+          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
             Password
           </label>
           <input
             type="password"
             placeholder="Create a password"
-            className="input input-bordered w-full"
+            className="input w-full border border-gray-700 bg-transparent rounded-md"
+            style={{ backgroundColor: "transparent" }}
           />
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="name">
+        <div className="mb-[3%]">
+          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
             Confirm password
           </label>
           <input
             type="password"
             placeholder="Confirm your password"
-            className="input input-bordered w-full"
+            className="input w-full border border-gray-700  rounded-md"
+            style={{ backgroundColor: "transparent" }}
           />
         </div>
 
-        <button type="submit" className="btn btn-primary w-full">
+        <button className="btn w-full bg-[#393E46] text-[20px] mb-[3%]">
           Sign Up
         </button>
-        <p>Or sign up with</p>
-        <button type="submit" className="btn btn-primary w-full">
-          Google
+
+        <div className="flex gap-5 justify-center text-center mb-[3%]">
+          <div
+            className="border-t-2 border-black my-4 flex-grow "
+            style={{ width: "400px" }}
+          ></div>
+
+          <p className="text-[20px] font-semibold">Or sign up with</p>
+          <div
+            className="border-t-2 border-black my-4 flex-grow"
+            style={{ width: "400px" }}
+          ></div>
+        </div>
+
+        <button className="btn bg-white w-full text-[#393E46] border-none text-[20px] mb-[3%]">
+          <FcGoogle style={{ fontSize: "1.5em" }} /> Google
         </button>
 
-        <p>Already have an account? Sign in</p>
+        <p className="text-center text-[20px] font-semibold text-black">
+          Already have an account? <span className="font-bold">Sign in</span>
+        </p>
       </form>
     </div>
   );
