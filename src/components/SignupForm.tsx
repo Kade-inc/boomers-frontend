08cc9618e6bf106873437e6779162de93ff557a2
MatoG50 <matog50@gmail.com>
2024-07-03 21:43:38 +0300

    form inputs and zod setup

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,15 +1,36 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FieldValues } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  username: z.string(),
+  password: z
+    .string()
+    .min(3, { message: "Password must be atleast three characters" }),
+  confirmpassword: z.string().min(3),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
     <div className="text-[#393E46]">
-      <form className="w-full px-24 py-[2%]">
+      <form className="w-full px-24 py-[2%]" onSubmit={handleSubmit(onSubmit)}>
         <p className="font-extrabold text-[50px] leading-[53px]">SIGN UP</p>
         <p className="py-[2.6%] text-[25px] font-semibold">
           Create an account to begin your journey
         </p>
         <div className="mb-[3%]">
-          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
+          <label className="block text-base font-bold mb-[1%]" htmlFor="email">
             Email
           </label>
           <input
@@ -17,43 +38,69 @@ const SignupForm = () => {
             placeholder="Enter your email"
             className="input w-full border border-gray-700 bg-transparent rounded-md"
             style={{ backgroundColor: "transparent" }}
+            {...register("email")}
+            id="email"
           />
         </div>
+        {errors.email && (
+          <p className="text-red-700">{errors.email?.message}</p>
+        )}
         <div className="mb-[3%]">
-          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
+          <label
+            className="block text-base font-bold mb-[1%]"
+            htmlFor="username"
+          >
             Username
           </label>
           <input
             type="text"
+            id="username"
             placeholder="Enter your username"
             className="input w-full border border-gray-700 bg-transparent rounded-md"
             style={{ backgroundColor: "transparent" }}
+            {...register("username")}
           />
         </div>
         <div className="mb-[3%]">
-          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
+          <label
+            className="block text-base font-bold mb-[1%]"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
             type="password"
+            autoComplete="on"
+            id="password"
             placeholder="Create a password"
             className="input w-full border border-gray-700 bg-transparent rounded-md"
             style={{ backgroundColor: "transparent" }}
+            {...register("password")}
           />
         </div>
         <div className="mb-[3%]">
-          <label className="block text-base font-bold mb-[1%]" htmlFor="name">
+          <label
+            className="block text-base font-bold mb-[1%]"
+            htmlFor="confirmpassword"
+          >
             Confirm password
           </label>
           <input
             type="password"
+            autoComplete="on"
+            id="confirmpassword"
             placeholder="Confirm your password"
             className="input w-full border border-gray-700  rounded-md"
             style={{ backgroundColor: "transparent" }}
+            {...register("confirmpassword")}
           />
         </div>
 
-        <button className="btn w-full bg-[#393E46] text-[20px] mb-[3%]">
+        <button
+          className="btn w-full bg-[#393E46] text-[20px] mb-[3%] disabled:text-gray-500"
+          type="submit"
+          disabled={!isValid}
+        >
           Sign Up
         </button>
 
@@ -70,7 +117,10 @@ const SignupForm = () => {
           ></div>
         </div>
 
-        <button className="btn bg-white w-full text-[#393E46] border-none text-[20px] mb-[3%]">
+        <button
+          className="btn bg-white w-full text-[#393E46] border-none text-[20px] mb-[3%]"
+          type="button"
+        >
           <FcGoogle style={{ fontSize: "1.5em" }} /> Google
         </button>
 
