75319726abe2cac20ef72c94d1f6fdbc994a1815
Paul Otieno <vitalispaul48@live.com>
2024-07-17 00:29:01 +0300

    Update Boomer Image

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,6 @@
 import image from "../assets/khamkeo-vilaysing-rpVQJbZMw8o-unsplash (1) 1.png";
 import SignupForm from "./SignupForm";
+import boomer from '../assets/boomer.svg';
 
 const Signup = () => {
   return (
@@ -7,10 +8,11 @@ const Signup = () => {
       <div className="hidden md:block w-[35%] relative">
         <img src={image} alt="image" className="w-full h-full object-cover" />
         <div className="absolute top-[4%] left-[5%] p-4 text-white font-normal text-[40px]">
-          <h1>LOGO</h1>
+          <h1 className="font-heading">LOGO</h1>
         </div>
-        <div className="border-8 rounded-[15px] absolute top-[28.6%] left-1/2 p-4 font-normal transform -translate-x-1/2 text-center text-[60px] w-[87%]">
-          WELCOME BOOMER!
+        <div className="absolute top-[28.6%] left-1/2 p-4  -translate-x-1/2 w-[87%]">
+
+          <img src={boomer} alt="image" className="" />
         </div>
       </div>
       <div className="w-full md:w-[65%] bg-[#F8B500]">
