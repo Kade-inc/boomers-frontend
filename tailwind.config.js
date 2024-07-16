edfc9440f719b609bac2257928b2854d5477c9c8
Paul Vitalis <vitalispaul48@live.com>
2024-07-16 19:27:44 +0300

    Fix/signup updates (#8) (#9)
    * Update placeholder color
    
    * Update error text colors
    
    * Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ export default {
       fontFamily: {
         'body': ["Montserrat", "sans-serif"],
         'heading': ["Changa One", "sans-serif"]
-      }
+      },
+      colors: {
+        'error': '#D92D2D',
+        'darkgrey': '#393E46',
+        'lightgrey': '#5C636E'
+      },
     },
   },
   plugins: [
