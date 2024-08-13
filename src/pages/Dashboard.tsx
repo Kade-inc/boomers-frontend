1336ff33947a5d5f21ad7e8b93a48c27a7bdbb43
Paul Otieno <vitalispaul48@live.com>
2024-08-14 01:57:04 +0300

    Add navigation bar component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import AdviceCard from "../components/AdviceCard";
+import NavigationBar from "../components/NavigationBar";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   return (
     <>
+    <NavigationBar />
       <div>Welcome Home</div>
       <button className="btn" onClick={() => navigate("/auth/login")}>
         Back
