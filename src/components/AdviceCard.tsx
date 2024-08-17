a1134f391941796699aadae5d19c22d56014c7c9
Ken Muchiri <kennethmuchiri7168@gmail.com>
2024-08-17 22:08:18 +0300

    Update AdviceCard.tsx

--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -1,10 +1,10 @@
 const AdviceCard = () => {
     return (
-    <div className="container mx-auto gap-5 max-w-[293px] h-[194px] p-[10px] left-[1095px] flex flex-col items-center justify-center bg-[#393e46] rounded-[5px]">
-        <div className="w-[173px] h-[23px] text-center text-white text-lg font-semibold font-['Montserrat']">Today’s advice</div>
-        <div className="w-[219px] h-[90px] text-center text-white text-[15px] font-medium font-['Montserrat']">The best time to start is now. Remember everything you’ve learnt and keep going.</div>
+    <div className="container mx-auto gap-5 max-w-[293px] h-[194px] p-[10px] left-[1095px] flex flex-col items-center justify-center bg-darkgrey rounded-[5px]">
+        <div className="w-[173px] h-[23px] text-center text-white text-lg font-semibold font-body">Today’s advice</div>
+        <div className="w-[219px] h-[90px] text-center text-white text-[15px] font-medium font-body">The best time to start is now. Remember everything you’ve learnt and keep going.</div>
         <div className="w-52 h-[21px] flex justify-center bg-[#f8b500] rounded-[50px] text-center">
-            <button className="text-center text-[#393e46] text-[10px] font-medium font-['Montserrat']">With ❤️ From Advice Slip Json API</button>
+            <button className="text-center text-darkgrey text-[10px] font-medium font-body">With ❤️ From Advice Slip Json API</button>
         </div>
     </div>
     );
