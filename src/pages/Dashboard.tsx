6e6004daeda7eb5d23ebb9d32225cafa0d81d006
Paul Otieno <vitalispaul48@live.com>
2024-08-20 02:22:36 +0300

    Update card

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -25,22 +25,22 @@ const Dashboard = () => {
       <ProfileCard/>      
       <div className="h-screen ml-3">
 
-        <div className="card bg-gradient-to-b from-teal-700 via-teal-650 to-teal-600 text-white w-[450px] h-[200px] rounded-[3px]">
+        <div className="card bg-gradient-to-b from-[#005E78] to-[#00989B] text-white w-[450px] h-[200px] rounded-[3px] font-body">
           <div className="card-body">
             <div className="flex justify-between w-full items-center">
-              <h2 className="font-medium ">Paul and the Funky Bunch</h2>
+              <h2 className="font-medium">Paul and the Funky Bunch</h2>
               <p className="text-right text-[12px]">Member</p>
             </div>
             <div className="flex justify-between w-full mt-[55px]">
-              <div className="flex-grow">
+              <div className="flex-grow text-[12px]">
                 <div>
                   {profile.domain} . {profile.subdomain} . {profile.topics[0]}{" "}
                   {additionalTopics > 0 && `+${additionalTopics}`}
                 </div>
                 <div>Very Active</div>
               </div>
-              <div className=" flex ml-auto">
-                <h2 className="font-normal rotate-[-90deg] origin-bottom-left whitespace-nowrap">
+              <div className=" flex mt-[-20px] mr-2.5">
+                <h2 className="font-normal rotate-[-90deg] origin-top-right whitespace-nowrap">
                   5 Members
                 </h2>
               </div>
