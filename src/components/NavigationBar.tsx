73d24b3e068740c1e7ac8b00ee4172a75139c6f5
Paul Otieno <vitalispaul48@live.com>
2024-08-21 19:09:08 +0300

    Update navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,15 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 // import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { useNavigate, NavLink } from "react-router-dom";
 
 function NavigationBar() {
+  const currentRoute = useLocation()
+  console.log("CURRENT: ", currentRoute)
+
+  const pathname = currentRoute.pathname
+
   return (
     <div className="navbar bg-base-100 flex md:px-5 justify-between px-5">
       <div className="flex p-0 w-[20%] md:w-[20%]">
@@ -13,40 +19,51 @@ function NavigationBar() {
         <MagnifyingGlassIcon className='inset-y-0 left-0 flex items-center pl-2 w-8 h-8 top-2.5' />
       </div>
       <div className="lg:w-[20%] justify-start items-center relative hidden md:flex">
-        <MagnifyingGlassIcon className='absolute inset-y-0 left-0 flex items-center pl-2 w-8 h-8 top-2.5' />
-        <input type="text" placeholder="Search team/users" className="input md:w-auto rounded-full pl-10 text-[12px] md:text-base" />
+        <MagnifyingGlassIcon className='absolute inset-y-0 left-0 flex items-center pl-2 w-8 h-8 top-2.5 fill-gray-400' />
+        <input type="text" placeholder="Search team/users" className="input md:w-auto rounded-full pl-10 text-[12px] md:text-base font-body bg-grey" />
       </div>
       
       
-      <div className="flex lg:w-[35%]">
-        <div className="hidden lg:flex">
-          <div className="flex items-center">
+      <div className="flex">
+        <div className="hidden lg:grid  lg:grid-flow-col lg:gap-8 lg:auto-rows-max lg:mr-[70px]">
+          <Link to="/" className={pathname === "/" ? "btn btn-ghost bg-yellow rounded-full" : "flex items-center"}>
+          <div className={"flex items-center text-[16px] font-body font-normal"}>
           
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
               <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
               <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
             </svg>
-            <Link className="btn btn-ghost text-xl font-body font-normal" to="/">Dashboard</Link>
+            <span className="ml-[10px]">
+            Dashboard
+            </span>
           </div>
-          <div className="flex items-center">
+          </Link>
+          <Link className={pathname === "/messages" ? "btn btn-ghost bg-yellow rounded-full" : "flex items-center"} to="/messages">
+          <div className={"flex items-center text-[16px] font-body font-normal"}>
               
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
               <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
               <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
             </svg>
-
-            <Link className="btn btn-ghost text-xl font-body font-normal" to="/messages">Messages</Link>
+            <span className="ml-[10px]">
+            Messages
+            </span>
           </div>
+          </Link>
 
-          <div className="flex items-center">
+            <Link className={pathname === "/teams" ? "btn btn-ghost bg-yellow rounded-full" : "flex items-center"} to="/teams">
+            <div className="flex items-center text-[16px] font-body font-normal">
               
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-              <path fillRule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clipRule="evenodd" />
-              <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
-            </svg>
-
-            <Link className="btn btn-ghost text-xl font-body font-normal" to="/teams">Teams</Link>
-          </div>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                <path fillRule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clipRule="evenodd" />
+                <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+              </svg>
+              <span className="ml-[10px]">
+              Teams
+              </span>
+              
+            </div>
+            </Link>
 
         </div>
 
