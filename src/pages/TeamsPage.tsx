a6f7e2a773fbd4ec88942a1530f4bf65cb7f1ca3
MatoG50 <matog50@gmail.com>
2024-09-04 20:35:09 +0300

    teams page init

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -1,5 +1,44 @@
+import TeamCard from "../components/TeamCard";
+
 const TeamsPage = () => {
-  return <div>TeamsPage</div>;
+  return (
+    <div className="h-screen text-darkgrey">
+      <p className="font-normal text-[20px] ">Text</p>
+
+      <div className="flex gap-2 flex-wrap">
+        <p>Filters:</p>
+        <select className="max-w-xs bg-transparent border border-1 w-[143px] p-1 style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
+          <option disabled selected>
+            Domain
+          </option>
+          <option>Game of Thrones</option>
+          <option>Lost</option>
+        </select>
+        <select className="max-w-xs bg-transparent border p-1 border-1 w-[143px] style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
+          <option disabled selected>
+            Sub domain
+          </option>
+          <option>Game of Thrones</option>
+          <option>Lost</option>
+        </select>
+        <select className="max-w-xs bg-transparent border p-1 border-1 w-[143px] style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
+          <option disabled selected>
+            Topics
+          </option>
+          <option>Game of Thrones</option>
+          <option>Lost</option>
+        </select>
+        <button className="w-[143px] text-[14px] p-1 text-white bg-red-600">
+          Clear all
+        </button>
+      </div>
+      <div className="mt-6 flex flex-wrap gap-3 m-auto">
+        <TeamCard />
+        <TeamCard />
+        <TeamCard />
+      </div>
+    </div>
+  );
 };
 
 export default TeamsPage;
