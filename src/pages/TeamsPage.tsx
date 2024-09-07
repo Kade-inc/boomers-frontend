df93c37b6b817f3f52464f7622bc27bc35411db5
MatoG50 <matog50@gmail.com>
2024-09-07 12:03:10 +0300

    fixes

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -2,36 +2,64 @@ import TeamCard from "../components/TeamCard";
 
 const TeamsPage = () => {
   return (
-    <div className="h-screen text-darkgrey">
-      <p className="font-normal text-[20px] ">Text</p>
+    <div className="h-screen text-darkgrey px-[20px]">
+      <p className="font-normal text-[20px] mt-3 mb-3">Text</p>
 
-      <div className="flex gap-2 flex-wrap">
-        <p>Filters:</p>
-        <select className="max-w-xs bg-transparent border border-1 w-[143px] p-1 style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
-          <option disabled selected>
-            Domain
-          </option>
-          <option>Game of Thrones</option>
-          <option>Lost</option>
-        </select>
-        <select className="max-w-xs bg-transparent border p-1 border-1 w-[143px] style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
-          <option disabled selected>
-            Sub domain
-          </option>
-          <option>Game of Thrones</option>
-          <option>Lost</option>
-        </select>
-        <select className="max-w-xs bg-transparent border p-1 border-1 w-[143px] style={{ borderColor: 'rgba(204, 205, 207, 1)' }} text-[14px]">
-          <option disabled selected>
-            Topics
-          </option>
-          <option>Game of Thrones</option>
-          <option>Lost</option>
-        </select>
-        <button className="w-[143px] text-[14px] p-1 text-white bg-red-600">
-          Clear all
-        </button>
+      <div className="flex gap-2 flex-wrap items-center justify-between">
+        <div className="flex gap-2 flex-wrap items-center">
+          <p>Filters:</p>
+          <select
+            className="max-w-xs bg-transparent border border-1 w-[143px] p-1 text-[14px]"
+            style={{ borderColor: "rgba(204, 205, 207, 1)" }}
+          >
+            <option disabled selected>
+              Domain
+            </option>
+            <option>Game of Thrones</option>
+            <option>Lost</option>
+          </select>
+          <select
+            className="max-w-xs bg-transparent border p-1 border-1 w-[143px] text-[14px]"
+            style={{ borderColor: "rgba(204, 205, 207, 1)" }}
+          >
+            <option disabled selected>
+              Sub domain
+            </option>
+            <option>Game of Thrones</option>
+            <option>Lost</option>
+          </select>
+          <select
+            className="max-w-xs bg-transparent border p-1 border-1 w-[143px] text-[14px]"
+            style={{ borderColor: "rgba(204, 205, 207, 1)" }}
+          >
+            <option disabled selected>
+              Topics
+            </option>
+            <option>Game of Thrones</option>
+            <option>Lost</option>
+          </select>
+          <button className="w-[143px] text-[14px] p-1 text-white bg-red-600">
+            Clear all
+          </button>
+        </div>
+
+        <label className="input input-bordered rounded-[50px] bg-transparent flex items-center gap-2 h-[29px] w-[200px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70 flex-shrink-0"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input type="text" className="" placeholder="Search" />
+        </label>
       </div>
+
       <div className="mt-6 flex flex-wrap gap-3 m-auto">
         <TeamCard />
         <TeamCard />
