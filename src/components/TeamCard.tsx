64ebd3d056b1150d0315f24081ebe3523c682a4e
Paul Otieno <vitalispaul48@live.com>
2024-10-24 01:16:31 +0300

    Fix team card errors

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,9 +2,13 @@ import { useNavigate } from "react-router-dom";
 import Team from "../entities/Team";
 import useAuthStore from "../stores/useAuthStore";
 
+type ExtendedTeamInterface = Team & {
+  subdomainTopics?: string[];
+};
+
 interface TeamProps {
-  team: Team;
-  styles: string;
+  team: ExtendedTeamInterface;
+  styles?: string;
   section?: string;
 }
 const TeamCard = ({ team, styles, section }: TeamProps) => {
@@ -44,16 +48,25 @@ const TeamCard = ({ team, styles, section }: TeamProps) => {
           </div>
           <div className="flex justify-between w-full">
             <div className="flex-grow text-[12px]">
-              <div className="flex items-center mb-2 font-medium">
-                {team.domain}
-                <div className="bg-white rounded-full w-1 h-1 mx-1"></div>
-                {team.subdomain}
-                <div className="bg-white rounded-full w-1 h-1 mx-1"></div>
-                {team.subdomainTopics[0]}
-                {team.subdomainTopics.length - 1 > 0 &&
-                  ` +${team.subdomainTopics.length - 1}`}
-              </div>
-              <div>Active</div>
+              {team && (
+                <>
+                 <div className="flex items-center mb-2 font-medium">
+                 {team.domain}
+                 <div className="bg-white rounded-full w-1 h-1 mx-1"></div>
+                 {team.subdomain}
+                 <div className="bg-white rounded-full w-1 h-1 mx-1"></div>
+                 {team?.subdomainTopics && (
+                  <>
+                    {team.subdomainTopics[0]}
+                    {team.subdomainTopics.length - 1 > 0 &&
+                   ` +${team.subdomainTopics.length - 1}`}
+                  </>
+                )}
+               </div>
+               <div>Active</div>
+               </>
+              )}
+             
             </div>
             {/* <div className="flex mt-[-20px] mr-2.5">
                 <h2 className="font-normal rotate-[-90deg] origin-top-right whitespace-nowrap">
