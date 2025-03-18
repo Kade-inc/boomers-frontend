95ef7ecf7f146bc75990741ae24d0804197a84e5
MatoG50 <matog50@gmail.com>
2025-03-18 20:17:15 +0300

    pending-requests ui

--- src/pages/PendingRequestsPage.tsx
+++ src/pages/PendingRequestsPage.tsx
@@ -1,17 +1,91 @@
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const PendingRequestsPage = () => {
   return (
-    <div className=" h-screen bg-base-100 px-10 pt-10">
-      <div className="">
+    <div className=" h-screen bg-base-100 px-10 pt-10 font-body">
+      <div className="text-[20px] font-semibold mb-[50px]">
         <p>
           <Link to="/">Dashboard</Link> &gt; Requests
         </p>
       </div>
-      <p>Pending Requests</p>
-      <div>
-        <p>Pending your action</p>
-        <div className="border-[1px] border-yellow w-[82px]">Expand</div>
+      <p className="text-[30px] font-medium mb-[40px]">Pending Requests</p>
+      <div className="flex gap-8 mb-[30px]">
+        <p className="text-[18px] font-semibold">Pending your action</p>
+        <div className="border-[1px] border-yellow w-[100px] flex items-center gap-3 text-[12px] justify-center rounded-[1px]">
+          Expand <MdOutlineKeyboardArrowDown className="size-4" />
+        </div>
+      </div>
+      <div className="flex gap-8 mb-[50px]">
+        <div className="w-[270px] bg-[#D9436D] h-[90px] rounded-[3px] pl-3 pt-3 text-white">
+          <p className="font-semibold text-[14px] mb-6">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-24">
+            <p>Paul Dreamer</p>
+            <button className="bg-yellow text-black w-[62px] h-[25px] rounded-[3px]">
+              Action
+            </button>
+          </div>
+        </div>
+        <div className="w-[270px] bg-[#D9436D] h-[90px] rounded-[3px] pl-3 pt-3 text-white">
+          <p className="font-semibold text-[14px] mb-6">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-24">
+            <p>Paul Dreamer</p>
+            <button className="bg-yellow text-black w-[62px] h-[25px] rounded-[3px]">
+              Action
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-8 mb-[30px] ">
+        <p className="text-[18px] font-semibold">Pending approvals</p>
+        <div className="border-[1px] border-yellow w-[100px] flex items-center gap-3 text-[12px] justify-center rounded-[1px]">
+          Collapse <MdOutlineKeyboardArrowDown className="size-4" />
+        </div>
+      </div>
+      <div className="flex gap-8 text-white mb-[50px]">
+        <div className="w-[270px] bg-yellow h-[90px] rounded-[3px] pl-3 pt-3">
+          <p className="font-semibold text-[14px] mb-7">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-28">
+            <p className="ml-[60%]">Paul Dreamer</p>
+          </div>
+        </div>
+        <div className="w-[270px] bg-yellow h-[90px] rounded-[3px] pl-3 pt-3">
+          <p className="font-semibold text-[14px] mb-7">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-28">
+            <p className="ml-[60%]">Paul Dreamer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-8 mb-[30px]">
+        <p className="text-[18px] font-semibold">Rejected Requests</p>
+        <div className="border-[1px] border-yellow w-[100px] flex items-center gap-3 text-[12px] justify-center rounded-[1px]">
+          Expand <MdOutlineKeyboardArrowDown className="size-4" />
+        </div>
+        <button className="bg-red-600 text-white text-[14px] w-[100px] h-[25px] rounded-[3px]">
+          Clear
+        </button>
+      </div>
+      <div className="flex gap-8">
+        <div className="w-[270px] bg-[#D9436D] h-[90px] rounded-[3px] pl-3 pt-3 text-white">
+          <p className="font-semibold text-[14px] mb-6">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-24">
+            <p>Paul Dreamer</p>
+            <button className="bg-yellow text-black w-[62px] h-[25px] rounded-[3px]">
+              Action
+            </button>
+          </div>
+        </div>
+        <div className="w-[270px] bg-[#D9436D] h-[90px] rounded-[3px] pl-3 pt-3 text-white">
+          <p className="font-semibold text-[14px] mb-6">Album cover</p>
+          <div className="text-[12px] font-medium flex gap-24">
+            <p>Paul Dreamer</p>
+            <button className="bg-yellow text-black w-[62px] h-[25px] rounded-[3px]">
+              Action
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
