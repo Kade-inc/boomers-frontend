d5ccce7f4be55d612254a257677aabbc7560b02e
Paul Otieno <vitalispaul48@live.com>
2025-05-18 02:53:17 +0300

    Fix profile redirect

--- src/pages/AllSearchResultsPage.tsx
+++ src/pages/AllSearchResultsPage.tsx
@@ -283,7 +283,7 @@ export default function AllResultsAllPage() {
                         )}
                       </div>
                     </div>
-                    <Link to={`/profile/${profile._id}`}>
+                    <Link to={`/profile/${profile.user_id}`}>
                       <button className="font-medium px-6 font-body bg-yellow border-none py-2 text-sm text-darkgrey rounded-full">
                         View
                       </button>
