8c67a1d3bea7eeef1f49d567301c52add8658835
Paul Otieno <vitalispaul48@live.com>
2026-01-18 21:58:45 +0300

    Add chat component

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -363,29 +363,29 @@ function NavigationBar() {
                   <span className={"ml-[10px]"}>Dashboard</span>
                 </div>
               </Link>
-              {/* <Link
-            className={
-              pathname === "/chat"
-                ? "btn btn-ghost bg-yellow rounded-full"
-                : "flex items-center"
-            }
-            to="/chat"
-          >
-            <div
-              className={`flex items-center text-[16px] font-body font-normal ${pathname === "/chat" ? "text-darkgrey" : ""}`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-6"
+              <Link
+                className={
+                  pathname === "/chat"
+                    ? "btn btn-ghost bg-yellow rounded-full"
+                    : "flex items-center"
+                }
+                to="/chat"
               >
-                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-              </svg>
-              <span className={"ml-[10px]"}>Messages</span>
-            </div>
-          </Link> */}
+                <div
+                  className={`flex items-center text-[16px] font-body font-normal ${pathname === "/chat" ? "text-darkgrey" : ""}`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6"
+                  >
+                    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                  </svg>
+                  <span className={"ml-[10px]"}>Messages</span>
+                </div>
+              </Link>
 
               <Link
                 className={
