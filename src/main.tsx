ff7898c3f56dabb73dc767d4fce5f7d6cdc53c79
Paul Otieno <vitalispaul48@live.com>
2026-02-04 00:21:47 +0300

    Setup sentry

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
-  sendDefaultPii: true
+  sendDefaultPii: true,
 });
 
 const queryClient = new QueryClient();
