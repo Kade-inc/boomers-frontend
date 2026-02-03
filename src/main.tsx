43283d17d247555d992b0acd78f9b5ad3f158d24
Paul Vitalis <vitalispaul48@live.com>
2026-02-04 00:23:55 +0300

    Setup sentry (#92)
    * Dockerize frontend
    
    * Setup Sentry
    
    * Setup sentry

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,14 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Modal from "react-modal";
 import { SocketProvider } from "./context/SocketContext";
 import { SSEProvider } from "./context/SSEContext";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 const queryClient = new QueryClient();
 
