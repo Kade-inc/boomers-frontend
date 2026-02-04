117f9e99f32533d225fef434e08878d81d03083b
Paul Otieno <vitalispaul48@live.com>
2026-02-04 04:58:46 +0300

    Remove hardcoded API_URL

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
 import * as Sentry from "@sentry/react";
 
-const API_URL = "http://localhost:5001/api";
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export interface LoginResponse {
   accessToken: string;
